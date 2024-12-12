e5e7a8dfe08527b5cb01c309cca1479333f1647e
americafrog <hemuhan2@gmail.com>
2024-12-12 16:53:16 +0900

    Add files via upload
    c#学生管理系统

---
+++ test_hmh/test_hmh/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_hmh
+{
+    internal class Circle:Shape
+    {
+     public int Radius {  get; set; }
+    public static int Count {  get; set; }
+
+        public Circle(string n,string d,int r)
+            : base(n, d) { 
+         Radius = r;
+            Count++;
+        
+        }
+      
+
+            
+        public override void ShowInfo()
+        {
+            Console.WriteLine("我是一个半径为{0}的{1}",Radius,name);
+        }
+
+    }
+}
---
+++ test_hmh/test_hmh/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace test_hmh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle obj = new Circle("圆形","circle",101);
+            obj.ShowInfo();
+            
+        }
+    }
+}
